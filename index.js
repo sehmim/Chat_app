fe7024951b0cb82f324f05549b457499b8b07ad2
sehmim <sehmim2010@gmail.com>
2018-04-28 19:14:22 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const socket = require('socket.io');
+// INIT Express
+const app = express();
+
+// Start server
+
+const server = app.listen(4000, ()=>{
+    console.log('LISTENING TO PORT 4000')
+})
+
+// STAIC FILES 
+app.use(express.static('public'));
+
+// Socket Setup
+const io = socket(server);
+// Connect socket with the server
+io.on('connection', (socket)=>{
+    console.log("made socket connection");
+})
---
+++ public/chat.js
@@ -0,0 +1,4 @@
+// MAKE CONNECTION
+
+// Client socket
+const socket = io.connect('http://localhost:4000');
